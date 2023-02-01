a0e98b42174c688dce2c11a2c4a6fc3453431a8e
romzyyy <fijay0117@gmail.com>
2023-02-02 00:05:08 +0700

    initialize project

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, {
+    Application,
+    Request,
+    Response,
+    NextFunction,
+    request,
+} from 'express';
+
+const app: Application = express();
+const port: Number = 4000;
+
+app.use('/health', (reg: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ status: '200' });
+});
+
+app.listen(port, () => console.log(`server is running on ${port}`));
