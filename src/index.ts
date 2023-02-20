e58d984019b0d3a71b32a641a32dd1b62832a7e1
romzyyy <fijay0117@gmail.com>
2023-02-20 12:58:58 +0700

    setup prettier and devDependescies

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
-import express, {
-    Application,
-    Request,
-    Response,
-    NextFunction,
-    request,
-} from 'express';
+import express, { Application, Request, Response, NextFunction, request } from 'express';
 
 const app: Application = express();
 const port: Number = 4000;
 
 app.use('/health', (reg: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ status: '200' });
+  res.status(200).send({ status: '200', data: 'hello world' });
 });
 
 app.listen(port, () => console.log(`server is running on ${port}`));
